e2f5aebad828daaeb29df1b3bc543398dc3dbbe9
Kevin Fernandes <kevinfrancisfernandes@gmail.com>
2024-02-04 18:49:14 +0530

    deleted unwanted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Food from "./Food";
-import Footer from "./Footer";
-import Header from "./Header"
+
 
 function App() {
     return (
       <>
-        <Header />
-        <Food />
-        <Footer />
+        
       </>
     );
 }
