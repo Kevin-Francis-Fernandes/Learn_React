5940afce785888813482e91e73e35542c66246bc
Kevin Fernandes <kevinfrancisfernandes@gmail.com>
2024-02-05 10:37:50 +0530

    event handlers

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,28 @@
 import "../styles/Button.css"
 function Button(){
-    const handleClick = () => { console.log("Hello"); };
-    
+
+    let count = 0;
+    const handleClick1 = () => { console.log("Hello"); };
 
     //when function has parameters use arrow function to pass to function to the handler
     const  handleClick2 = (message) => ()=>{console.log(message)}; 
     
+    const handleClick3 = (name) => {
+        if(count < 3){
+            count++;
+            console.log(`${name} you clicked me ${count} time/s`);
+        }
+        else{
+            console.log(`${name} stop clicking me! `);
+        }
+    }
+    //changing when clicked 
+    const handleClick = (e) => e.target.textContent = "hi";
+    
+
     return (
         <>
-            <button onClick={handleClick2("Kevin")} className='button'>Click Me</button>
+            <button onClick={(e)=>handleClick(e)} className='button'>Click Me</button>
         </>
     );
 }
